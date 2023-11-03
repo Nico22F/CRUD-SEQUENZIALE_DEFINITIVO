35fa216e9dbaadb2f54bb99b8709867e9d7c1010
Nicolò Fabbricatore <nicofabbricatore22@gmail.com>
2023-11-03 11:02:48 +0100

    Aggiungere i file di progetto.

---
+++ CRUD-SEQUENZIALE_DEFINITIVO/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CRUD_SEQUENZIALE_DEFINITIVO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aggiungi_prodotto = new System.Windows.Forms.Button();
+            this.modifica_prodotto = new System.Windows.Forms.Button();
+            this.elimina_prodotto = new System.Windows.Forms.Button();
+            this.resetta_file = new System.Windows.Forms.Button();
+            this.apri_file = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // aggiungi_prodotto
+            // 
+            this.aggiungi_prodotto.Location = new System.Drawing.Point(122, 77);
+            this.aggiungi_prodotto.Name = "aggiungi_prodotto";
+            this.aggiungi_prodotto.Size = new System.Drawing.Size(163, 76);
+            this.aggiungi_prodotto.TabIndex = 0;
+            this.aggiungi_prodotto.Text = "AGGIUNGI PRODOTTO";
+            this.aggiungi_prodotto.UseVisualStyleBackColor = true;
+            this.aggiungi_prodotto.Click += new System.EventHandler(this.aggiungi_prodotto_Click);
+            // 
+            // modifica_prodotto
+            // 
+            this.modifica_prodotto.Location = new System.Drawing.Point(122, 159);
+            this.modifica_prodotto.Name = "modifica_prodotto";
+            this.modifica_prodotto.Size = new System.Drawing.Size(163, 76);
+            this.modifica_prodotto.TabIndex = 1;
+            this.modifica_prodotto.Text = "MODIFICA PRODOTTO";
+            this.modifica_prodotto.UseVisualStyleBackColor = true;
+            this.modifica_prodotto.Click += new System.EventHandler(this.modifica_prodotto_Click);
+            // 
+            // elimina_prodotto
+            // 
+            this.elimina_prodotto.Location = new System.Drawing.Point(122, 241);
+            this.elimina_prodotto.Name = "elimina_prodotto";
+            this.elimina_prodotto.Size = new System.Drawing.Size(163, 76);
+            this.elimina_prodotto.TabIndex = 2;
+            this.elimina_prodotto.Text = "ELIMINA PRODOTTO";
+            this.elimina_prodotto.UseVisualStyleBackColor = true;
+            this.elimina_prodotto.Click += new System.EventHandler(this.elimina_prodotto_Click);
+            // 
+            // resetta_file
+            // 
+            this.resetta_file.Location = new System.Drawing.Point(122, 323);
+            this.resetta_file.Name = "resetta_file";
+            this.resetta_file.Size = new System.Drawing.Size(163, 76);
+            this.resetta_file.TabIndex = 3;
+            this.resetta_file.Text = "RESETTA FILE";
+            this.resetta_file.UseVisualStyleBackColor = true;
+            this.resetta_file.Click += new System.EventHandler(this.resetta_file_Click);
+            // 
+            // apri_file
+            // 
+            this.apri_file.Location = new System.Drawing.Point(12, 420);
+            this.apri_file.Name = "apri_file";
+            this.apri_file.Size = new System.Drawing.Size(358, 165);
+            this.apri_file.TabIndex = 4;
+            this.apri_file.Text = "VISUALIZZA FILE";
+            this.apri_file.UseVisualStyleBackColor = true;
+            this.apri_file.Click += new System.EventHandler(this.apri_file_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "CRUD CON ACCESSO SEQUENZIALE22F";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 597);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.apri_file);
+            this.Controls.Add(this.resetta_file);
+            this.Controls.Add(this.elimina_prodotto);
+            this.Controls.Add(this.modifica_prodotto);
+            this.Controls.Add(this.aggiungi_prodotto);
+            this.Name = "Form1";
+            this.Text = "FINESTRA22F";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button aggiungi_prodotto;
+        private System.Windows.Forms.Button modifica_prodotto;
+        private System.Windows.Forms.Button elimina_prodotto;
+        private System.Windows.Forms.Button resetta_file;
+        private System.Windows.Forms.Button apri_file;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CRUD-SEQUENZIALE_DEFINITIVO/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace CRUD_SEQUENZIALE_DEFINITIVO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // controllo che il file sia presente. In caso contrario, lo creo
+
+            if (File.Exists("./file_crud.dat") == false)
+            {
+                ResetFile();
+            }
+        }
+
+        // FUNZIONI UTILIZZATE
+
+        public void ResetFile()
+        {
+            FileStream file = new FileStream("./file_crud.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(file);
+            string prodotto = "@", prezzo = "@";
+            byte[] strInByte;
+
+            string riga = prodotto.PadRight(32) + prezzo.PadRight(32);
+            strInByte = Encoding.Default.GetBytes(riga);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                bw.Write(strInByte);
+            }
+
+            file.Close();
+            bw.Close();
+        }
+
+        public void AggiungiProdotto(string prezzo, string nome)
+        {
+            FileStream file = new FileStream("./file_crud.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(file);
+
+            // nome prodotto
+
+            string riga = nome.PadRight(32) + prezzo.PadRight(32);
+            byte[] strInByte = Encoding.Default.GetBytes(riga);
+            bw.BaseStream.Seek((counter_riga - 1) * size, SeekOrigin.Begin);
+            bw.Write(strInByte);
+
+            counter_riga++;
+
+            file.Close();
+            bw.Close();
+        }
+
+        // array che conterrà i nomi e le posizioni
+
+        public string[] nomiprodotti = new string[100];
+        public int dim; // posizione 
+       
+        // variabili pubbliche
+
+        public int counter_riga = 1;
+        const int size = 64;
+
+
+        // aggiunge un prodotto alla lista
+        private void aggiungi_prodotto_Click(object sender, EventArgs e)
+        {
+            // input box nome e do while per input corretto
+            // nel caso l'utente esca quando inserisce il nome oppure lascia il campo vuoto, non verrà chiesto il prezzo
+
+
+            string titolo_input = "Aggiungi Prodotto - NOME", esempio = "nome prodotto", frase = "Inserisci il nome del prodotto che vuoi aggiungere";
+            object input_aggiungiprodotto = Interaction.InputBox(frase, titolo_input, esempio);
+
+            if ((string)input_aggiungiprodotto == "") // user esce o lascia il campo vuoto
+            {
+                MessageBox.Show("Errore nell'aggiunta del prodotto", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else
+            {
+                nomiprodotti[dim] = (string)input_aggiungiprodotto;
+                string nome_temporaneo = (string)input_aggiungiprodotto; // salvo il nome in una variabile temporanea, verrà scritto nel file solo se il prezzo verrà scritto nel modo corretto
+
+                // input prezzo
+
+                titolo_input = "Aggiungi Prodotto - Prezzo"; esempio = "prezzo prodotto"; frase = "Inserisci il prezzo del prodotto che vuoi aggiungere";
+                input_aggiungiprodotto = Interaction.InputBox(frase, titolo_input, esempio);
+                int prova_numero = 0; // se la conversione risultasse corretta (questa sotto) la stringa convertita in float finirebbe qua
+
+                // tryparse serve per vedere se la conversione funziona
+                if (!int.TryParse((string)input_aggiungiprodotto, out prova_numero))
+                {
+                    MessageBox.Show("Errore nell'aggiunta del prodotto", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    nomiprodotti[dim] = null;
+                }
+                else
+                {
+                    // se non da nessun errore, dim (posizione) aumenta e viene aggiunto il prodotto al file
+
+                    dim++;
+
+                    // aggiunta del prodotto su file
+
+                    AggiungiProdotto((string)input_aggiungiprodotto, nome_temporaneo);
+                }
+            }
+        }
+
+        private void modifica_prodotto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void elimina_prodotto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // resetta il file e riempie tutto con @
+        private void resetta_file_Click(object sender, EventArgs e)
+        {
+            // prima di resettare chiede una conferma dall'utente
+
+            DialogResult ConfermaReset = MessageBox.Show("Vuoi resettare l'intero file?", "RESET FILE", MessageBoxButtons.YesNo);
+
+            if (ConfermaReset == DialogResult.Yes) // se l'utente clicca si
+            {
+                ResetFile();
+            }
+        }
+
+        // quando cliccato, apre il file dove sono visualizzati tutti i prodotti
+        private void apri_file_Click(object sender, EventArgs e)
+        {
+            // trova il percorso del file
+
+            string percorsoFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "file_crud.dat");
+
+            // apre il file
+
+            Process.Start(percorsoFile);
+        }
+
+    }
+}
